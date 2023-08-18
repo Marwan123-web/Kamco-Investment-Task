affbe0ed63b67fe9c4c8d69cdf671cc858b903ee
Marawan Salman <msalman@kashier.io>
2023-08-18 20:39:42 +0300

    finished select comp

---
+++ src/shared/components/select/Select.tsx
@@ -0,0 +1,44 @@
+import "./Select.scss";
+const Select = ({
+    id,
+    label,
+    value,
+    classes,
+    options,
+    changeFun,
+}: {
+    id: string;
+    label: string;
+    value?: string;
+    classes?: string;
+    options: { id: number; name: string; value: string }[];
+    changeFun?: Function;
+}) => {
+    const RunChangeFun = (e: any) => {
+        if (changeFun) {
+            changeFun(e.target.value);
+        }
+    };
+    return (
+        <div className="select">
+            <label htmlFor="select-list">{label}</label>
+            <select
+                id="select-list"
+                className={classes}
+                value={value}
+                defaultValue={""}
+                onChange={(e) => RunChangeFun(e)}
+            >
+                {options.map((option: { id: number; name: string; value: string }) => {
+                    return (
+                        <option value={option.value} key={option.id}>
+                            {option.name}
+                        </option>
+                    );
+                })}
+            </select>
+        </div>
+    );
+};
+
+export default Select;
